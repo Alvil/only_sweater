b250341ab3aeaf6c1083167b577ae5de9e09e316
Alvil <alvil.balbuena@gmail.com>
2023-01-28 06:28:56 +0800

    fix footer spacing

--- src/components/organisms/FooterSection.tsx
+++ src/components/organisms/FooterSection.tsx
@@ -8,7 +8,7 @@ export const FooterSection: FC = () => {
   const links = ["about", "item", "shop", "info", "press"];
 
   return (
-    <Container className="w-75 d-flex justify-content-between align-items-center libre-baskerville">
+    <Container className="w-75 d-flex justify-content-between align-items-center libre-baskerville ps-5 pe-5">
       <div>
         <img
           src="only_sweater_slogan.png"
