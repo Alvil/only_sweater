57f29dfa59494d9d77fe748e38474d4e2ed747e0
Alvil <alvil.balbuena@gmail.com>
2023-01-28 06:17:37 +0800

    fix about section

--- src/components/organisms/AboutSection.tsx
+++ src/components/organisms/AboutSection.tsx
@@ -1,30 +1,35 @@
-import { ViewMore } from "components/atoms";
 import React, { FC } from "react";
-import { Card, Container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import styles from "styles/components/About.module.scss";
+import { ViewMore } from "components/atoms";
 
 export const AboutSection: FC = () => {
   return (
     <Container fluid className="d-flex justify-content-center">
-      <img src="sweater_4.png" alt="sweater_image_5" height={455} />
-      <div style={{ maxWidth: "300px" }}>
-        <Card className="border-0">
-          <Card.Title className="text-center text-dark libre-baskerville">
+      <img
+        src="sweater_4.png"
+        alt="sweater_image_5"
+        height={455}
+        className="me-3"
+      />
+      <div className={styles.about_container}>
+        <div className={styles.about_content}>
+          <p className="fs-2 libre-baskerville text-dark pb-2 text-center">
             About
-          </Card.Title>
-          <Card.Text>
+          </p>
+          <p>
             着るだけで心が満たされていくそんなセーターを作るために
             セーターだけを販売するOnly Sweaterを立ち上げました。
-          </Card.Text>
-          <Card.Text>
+          </p>
+          <p>
             冬の季節にしか着ないセーターだけれど、
             少しこだわるだけで今年の冬を特別なものにしてもらえるように
             細部までこだわって作りました。
-          </Card.Text>
-          <Card.Text>あなたの冬が特別なものになりますように...</Card.Text>
-          <Card.Text>
-            <ViewMore href="/about" />
-          </Card.Text>
-        </Card>
+          </p>
+          <p>あなたの冬が特別なものになりますように...</p>
+
+          <ViewMore href="/about" />
+        </div>
       </div>
     </Container>
   );
