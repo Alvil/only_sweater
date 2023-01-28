254f56529dd83938ab02fd5ad42fb8d96143764a
Alvil <alvil.balbuena@gmail.com>
2023-01-28 19:26:32 +0800

    responsive footer

--- src/components/organisms/FooterSection.tsx
+++ src/components/organisms/FooterSection.tsx
@@ -3,17 +3,33 @@ import styles from "styles/components/Footer.module.scss";
 
 export const FooterSection: FC = () => {
   return (
-    <div style={{ position: "relative" }}>
-      <img
-        src="sweater_room.png"
-        alt="sweater_room"
-        className="w-100 opacity-50"
-      />
-      <img
-        src="only_sweater_slogan_full.png"
-        alt="only_sweater_slogan"
-        className={styles.sweater_slogan}
-      />
-    </div>
+    <>
+      {/* screen >= 768 */}
+      <div className={styles.wide_screen_wrapper}>
+        <img
+          src="sweater_room.png"
+          alt="sweater_room"
+          className="w-100 opacity-50"
+        />
+        <img
+          src="only_sweater_slogan_full.png"
+          alt="only_sweater_slogan_full"
+          className={styles.sweater_slogan}
+        />
+      </div>
+      {/* screen < 768 */}
+      <div className={styles.narrow_screen_wrapper}>
+        <img
+          src="sweater_room_single.png"
+          alt="sweater_room_single"
+          className="w-100 opacity-50"
+        />
+        <img
+          src="only_sweater_slogan_full.png"
+          alt="only_sweater_slogan_full"
+          className={styles.sweater_slogan}
+        />
+      </div>
+    </>
   );
 };
