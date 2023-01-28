4215c812a07d8345759cde09fbbaaf5548aad793
Alvil <alvil.balbuena@gmail.com>
2023-01-28 20:03:44 +0800

    add background to nav burger

--- src/components/organisms/NavigationBar.tsx
+++ src/components/organisms/NavigationBar.tsx
@@ -6,39 +6,45 @@ import { FacebookIcon, InstagramIcon, OnlySweatersLogo } from "../atoms";
 export const NavigationBar: FC = () => {
   // TODO: fix navbar on sm display
   return (
-    <Navbar collapseOnSelect expand="md" variant="light">
-      <Container fluid className="libre-baskerville">
-        <Navbar.Brand href="/" className="text-center">
-          <OnlySweatersLogo />
-          <span className={styles.tag_line}>We sell only sweaters</span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav d-flex">
-          <Nav className="justify-content-end ms-auto">
-            <Nav.Link href="/about" className={styles.link}>
-              About
-            </Nav.Link>
-            <Nav.Link href="/item" className={styles.link}>
-              Item
-            </Nav.Link>
-            <Nav.Link href="/shop" className={styles.link}>
-              Shop
-            </Nav.Link>
-            <Nav.Link href="/info" className={styles.link}>
-              Info
-            </Nav.Link>
-            <Nav.Link href="/press" className={styles.link}>
-              Press
-            </Nav.Link>
-            <Nav.Link href="#instagram" className="ms-5">
-              <InstagramIcon />
-            </Nav.Link>
-            <Nav.Link href="#fb">
-              <FacebookIcon />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      variant="light"
+      className="libre-baskerville"
+    >
+      <Navbar.Brand href="/" className="text-center ms-3">
+        <OnlySweatersLogo />
+        <span className={styles.tag_line}>We sell only sweaters</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        className={styles.navbar_collapse}
+      >
+        <Nav className="justify-content-end ms-auto">
+          <Nav.Link href="/about" className={styles.link}>
+            About
+          </Nav.Link>
+          <Nav.Link href="/item" className={styles.link}>
+            Item
+          </Nav.Link>
+          <Nav.Link href="/shop" className={styles.link}>
+            Shop
+          </Nav.Link>
+          <Nav.Link href="/info" className={styles.link}>
+            Info
+          </Nav.Link>
+          <Nav.Link href="/press" className={styles.link}>
+            Press
+          </Nav.Link>
+          <Nav.Link href="#instagram" className="ms-5">
+            <InstagramIcon />
+          </Nav.Link>
+          <Nav.Link href="#fb">
+            <FacebookIcon />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
