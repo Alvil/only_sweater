c98150e1ebbefb03c137fa88c13671867dbddccb
Alvil <alvil.balbuena@gmail.com>
2023-01-28 17:51:05 +0800

    responsive sub footer

--- src/components/organisms/SubFooterSection.tsx
+++ src/components/organisms/SubFooterSection.tsx
@@ -8,39 +8,80 @@ export const SubFooterSection: FC = () => {
   const links = ["about", "item", "shop", "info", "press"];
 
   return (
-    <Container className="w-75 libre-baskerville">
-      <div
-        className="d-flex justify-content-between align-items-center px-5"
-        style={{ paddingBottom: "8rem" }}
-      >
-        <div>
+    <Container fluid className="libre-baskerville">
+      {/* screen width >=1400 */}
+      <div className={styles.wide_screen_wrapper}>
+        <div
+          className="d-flex justify-content-between align-items-center px-5"
+          style={{ paddingBottom: "8rem" }}
+        >
+          <div>
+            <img
+              src="only_sweater_slogan.png"
+              alt="sweater_slogan"
+              style={{ width: "200px" }}
+            />
+            We sell only sweaters
+          </div>
+          <div>
+            {links.map((link) => (
+              <a href={`/${link}`} className={styles.text_link}>
+                {capitalize(link)}
+              </a>
+            ))}
+            <a href="#instagram" className="ms-5">
+              <InstagramLogo />
+            </a>
+            <a href="#facebook" className="m-3">
+              <FacebookLogo />
+            </a>
+          </div>
+        </div>
+        <div className="border-bottom mb-1 pb-4">
+          <div className="pb-1">Tel: 0312345678</div>
+          <div>Email: onlysweater@jp.com</div>
+        </div>
+        <div className="text-center m-3" style={{ fontSize: ".85em" }}>
+          <span>2022@only sweater</span>
+        </div>
+      </div>
+      {/* screen width <1400 */}
+      <div className={styles.narrow_screen_wrapper}>
+        <div className="text-center py-5">
           <img
             src="only_sweater_slogan.png"
             alt="sweater_slogan"
             style={{ width: "200px" }}
           />
-          We sell only sweaters
+          <div className={styles.slogan_text}>We sell only sweaters</div>
+          <div className={styles.links_wrapper}>
+            {links.map((link) => (
+              <div className="col-4">
+                <a href={`/${link}`} className={styles.text_link}>
+                  {capitalize(link)}
+                </a>
+              </div>
+            ))}
+            <div className="col-4">
+              <a href="#instagram" className="me-2">
+                <InstagramLogo />
+              </a>
+              <a href="#facebook">
+                <FacebookLogo />
+              </a>
+            </div>
+          </div>
         </div>
-        <div>
-          {links.map((link) => (
-            <a href={`/${link}`} className={styles.text_link}>
-              {capitalize(link)}
-            </a>
-          ))}
-          <a href="#instagram" className="ms-5">
-            <InstagramLogo />
-          </a>
-          <a href="#facebook" className="m-3">
-            <FacebookLogo />
-          </a>
+        <div className=" mb-1 text-center">
+          <div className="pb-3">Tel: 0312345678</div>
+          <div className="pb-5">Email: onlysweater@jp.com</div>
         </div>
-      </div>
-      <div className="border-bottom mb-1 pb-4">
-        <div className="pb-1">Tel: 0312345678</div>
-        <div>Email: onlysweater@jp.com</div>
-      </div>
-      <div className="text-center m-3" style={{ fontSize: ".85em" }}>
-        <span>2022@only sweater</span>
+        <div
+          className="text-center m-3 pt-3 border-top w-100"
+          style={{ fontSize: ".85em" }}
+        >
+          <span>2022@only sweater</span>
+        </div>
       </div>
     </Container>
   );
